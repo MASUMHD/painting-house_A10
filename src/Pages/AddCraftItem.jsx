9a48d78d7bf13d5a461e519d762e9761e99c9a1b
MASUMHD <mrmasum2k4@gmail.com>
2024-04-27 12:10:38 +0600

    Add Craft Item from code push

--- src/Pages/AddCraftItem.jsx
+++ src/Pages/AddCraftItem.jsx
@@ -1,10 +1,157 @@
-
 const AddCraftItem = () => {
-    return (
-        <div>
-            <h1>This is AddCraftItem</h1>
-        </div>
-    );
+  return (
+    <div className="mt-6 mb-8">
+      <section className="p-6 rounded-2xl bg-emerald-200">
+        <form
+          noValidate=""
+          action=""
+          className="container flex flex-col mx-auto space-y-12"
+        >
+          <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm bg-sky-300">
+            <div className="space-y-2 col-span-full lg:col-span-1 text-center mt-0 md:mt-32">
+              <p className="font-bold text-3xl ">Painting house</p>
+              <p className="text-lg">
+              Commencing house painting. Colors swirl, brushes dance, walls narrate tales, turning home into a canvas, vibrant with life.
+              </p>
+            </div>
+            <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
+              
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="firstname" className="text-sm">
+                  First name
+                </label>
+                <input
+                  id="firstname"
+                  type="text"
+                  placeholder="First name"
+                  className="w-full p-2 rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-default-600 dark:border-gray-300"
+                  fdprocessedid="joguop"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="lastname" className="text-sm">
+                  Last name
+                </label>
+                <input
+                  id="lastname"
+                  type="text"
+                  placeholder="Last name"
+                  className="w-full p-2 rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-default-600 dark:border-gray-300"
+                  fdprocessedid="qvp45n"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="firstname" className="text-sm">
+                  First name
+                </label>
+                <input
+                  id="firstname"
+                  type="text"
+                  placeholder="First name"
+                  className="w-full p-2 rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-default-600 dark:border-gray-300"
+                  fdprocessedid="joguop"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="lastname" className="text-sm">
+                  Last name
+                </label>
+                <input
+                  id="lastname"
+                  type="text"
+                  placeholder="Last name"
+                  className="w-full p-2 rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-default-600 dark:border-gray-300"
+                  fdprocessedid="qvp45n"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="firstname" className="text-sm">
+                  First name
+                </label>
+                <input
+                  id="firstname"
+                  type="text"
+                  placeholder="First name"
+                  className="w-full p-2 rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-default-600 dark:border-gray-300"
+                  fdprocessedid="joguop"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="lastname" className="text-sm">
+                  Last name
+                </label>
+                <input
+                  id="lastname"
+                  type="text"
+                  placeholder="Last name"
+                  className="w-full p-2 rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-default-600 dark:border-gray-300"
+                  fdprocessedid="qvp45n"
+                />
+              </div>
+              
+              <div className="col-span-full">
+                <label htmlFor="address" className="text-sm">
+                  Address
+                </label>
+                <input
+                  id="address"
+                  type="text"
+                  placeholder=" Address"
+                  className="w-full p-2 rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-default-600 dark:border-gray-300"
+                  fdprocessedid="9o4c6o"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-2">
+                <label htmlFor="city" className="text-sm">
+                  City
+                </label>
+                <input
+                  id="city"
+                  type="text"
+                  placeholder=" City"
+                  className="w-full p-2 rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-default-600 dark:border-gray-300"
+                  fdprocessedid="8exqeg"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-2">
+                <label htmlFor="state" className="text-sm">
+                  State / Province
+                </label>
+                <input
+                  id="state"
+                  type="text"
+                  placeholder=" State / Province"
+                  className="w-full p-2 rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-default-600 dark:border-gray-300"
+                  fdprocessedid="e2hf8m"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-2">
+                <label htmlFor="zip" className="text-sm">
+                  ZIP / Postal
+                </label>
+                <input
+                  id="zip"
+                  type="text"
+                  placeholder=" ZIP / Postal"
+                  className="w-full p-2 rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-default-600 dark:border-gray-300"
+                  fdprocessedid="k2ipla"
+                />
+              </div>
+
+              <div className="col-span-full">
+                <input className="btn bg-rose-500 w-full" type="submit" value="Submit" />
+              </div>
+              
+              
+            </div>
+            
+          </fieldset>
+          
+          
+        </form>
+      </section>
+    </div>
+  );
 };
 
 export default AddCraftItem;
